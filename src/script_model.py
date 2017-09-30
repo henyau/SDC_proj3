6204cd2b7531c328e10655dd13517b6b9771e8d9
Henry Yau <henyau@gmail.com>
2017-09-30 01:10:51 -0400

    added generator for training data

--- src/script_model.py
+++ src/script_model.py
@@ -2,9 +2,10 @@
 # Keras model based on nVidia paper "End to End Learning for SDC"
 # Running on a car simulator, training with only video from cameras and steering wheel, throttle, and brake inputs
 # 9/29/17: first go using a simple model, gets halfway around track 1
+# 9/30/17: added a generator to create batches of training data
 # todo: 
 # 1.) Gather more training data. Driving on road backwards, getting back to center of lane and track 2
-# 2.) Need to use generators, running out of memory. also might need to use AWS or googleCC if need to increase parameters
+# 2.)(Done) Need to use generators, running out of memory . also might need to use AWS or googleCC if need to increase parameters
 # 3.) Incorporate throttle and brake inputs
 # 4.) Controller is just a simple PI controller, should probably modify that
 
@@ -27,19 +28,22 @@ def generator(samples, batch_size=32):
         for offset in range(0, num_samples, batch_size):
             batch_samples = samples[offset:offset+batch_size]
 
-            images = []
-            angles = []
+            image_list = []
+            steering_angle_list = []
             for batch_sample in batch_samples:
                 name = '../data/IMG/'+batch_sample[0].split('/')[-1]
                 center_image = cv2.imread(name)
                 center_angle = float(batch_sample[3])
-                images.append(center_image)
-                angles.append(center_angle)
-
-            # trim image to only see section with road
-            X_train = np.array(images)
-            y_train = np.array(angles)
-            yield sklearn.utils.shuffle(X_train, y_train)
+                image_list.append(center_image)
+                steering_angle_list.append(center_angle)
+                
+                #include a flipped shot
+                image_list.append(np.fliplr(center_image)) 
+                steering_angle_list.append(center_angle*-1.0)
+                
+            X_train = np.array(image_list)
+            y_train = np.array(steering_angle_list)
+            yield sklearn.utils.shuffle(X_train, y_train) #return a subset for training
             
 lines = []
 with open('../data/driving_log.csv') as csvfile:
@@ -51,42 +55,8 @@ with open('../data/driving_log.csv') as csvfile:
 train_samples, validation_samples = train_test_split(lines, test_size=0.2)
 
 
-image_list = []
-actuator_list = []
-for line in lines:
-    filename_center = line[0].split('/')[-1]
-    filename_left = line[1].split('/')[-1]
-    filename_right = line[2].split('/')[-1]
-    current_path_center = '../data/IMG/' + filename_center
-    current_path_left = '../data/IMG/' + filename_left
-    current_path_right = '../data/IMG/' + filename_right
-    
-    image = cv2.imread(current_path_center)
-    image_list.append(image)
-##    image = cv2.imread(current_path_left)
-##    image_list.append(image)
-##    image = cv2.imread(current_path_right)
-##    image_list.append(image)
-
-    
-    actuator = float(line[3])
-    correction = 0.05
-    
-    actuator_list.append(actuator)
-    
-    ##--add flipped versions
-    image_list.append(np.fliplr(image))
-    actuator_list.append(actuator*-1.0)
-    
-##    actuator_list.append(actuator-correction)
-##    actuator_list.append(actuator+correction)
-
-X_train = np.array(image_list)
-y_train = np.array(actuator_list)
-
 train_generator = generator(train_samples, batch_size=32)
 validation_generator = generator(validation_samples, batch_size=32)
-
 
 #Keras model based on nVidia paper "End to End Learning for SDC"
 model = Sequential()
